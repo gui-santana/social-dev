f2b84e8a73167b1e16ea05cf7ac9a1689876ff13
Guilherme Santana <business@guilhermesantana.com.br>
2025-07-04 17:13:27 -0300

    Criação das primeiras rotas de teste e perfil

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre', function () {
+    return '<h1>Página Sobre a SocialDev</h1><p>Esta é uma rede social para desenvolvedores!</p>';
+});
+
+Route::get('/perfil/{username}', function ($username) {
+    return "Você está vendo o perfil de: " . htmlspecialchars($username);
+});
